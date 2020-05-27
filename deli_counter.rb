8bf2e50d1340e44e897b69f2de72e40632ff355c
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 15:56:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(katz_deli)
   if katz_deli.size == 0 
     "The line is currently empty."
   else 
-    
+    count = 0 
+    while count < katz_deli.size
     katz_deli.each_with_index do |customer, index| 
       puts "#{index}. #{customer}"
     end
