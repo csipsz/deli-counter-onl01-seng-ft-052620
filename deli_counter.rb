23c62435b8af967cb73d19222becbbdd267c088e
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 16:14:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
-    count = 1 
+    count = 0 
     result = 'The line is currently: '
     while count < katz_deli.size
-      result += "#{count.to_s}. #{katz_deli[count]} "
+      result += "#{count.to_s + 1}. #{katz_deli[count]} "
       count += 1 
     end
     puts result
